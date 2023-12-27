4df0bf791b72a599f9db745401ef3a93ddd1c88f
nguyenquoctrung <quoctrung160723@gmail.com>
2023-12-27 19:56:03 +0700

    Trung

---
+++ src/Utils/Date.js
@@ -0,0 +1,72 @@
+export const fruits = [
+  {
+    name: "IPhone 13 ProMax",
+    pieces: "7",
+    price: 4.939,
+    img: "https://c1.lestechnophiles.com/images.frandroid.com/wp-content/uploads/2021/09/apple-iphone-13-pro-frandroid-2021.png?resize=170",
+  },
+  {
+    name: "IPhone 13",
+    pieces: "7",
+    price: 4.999,
+    img: "https://c0.lestechnophiles.com/images.frandroid.com/wp-content/uploads/2021/09/apple-iphone-13-frandroid-2021.png?resize=170",
+  },
+  {
+    name: "IPhone 14 ProMax",
+    pieces: "6",
+    price: 1.999,
+    img: "https://www.telepolis.pl/images/katalog/Apple/Apple_iPhone_14_Pro_Max/apple-iphone-14-pro-max-2.jpg",
+  },
+  
+  
+  {
+    name: "IPhone 13",
+    pieces: "5",
+    price: 4.999,
+    img: "https://mobilepriceall.com/wp-content/uploads/2022/09/Apple-iPhone-14-1024x1024.jpg",
+  },
+];
+export const vegetables = [
+  {
+    name: "Apple AirPods ",
+    pieces: "10",
+    price: 1.999,
+    img: "https://www.crystalcleartelecom.uk/wp-content/uploads/2020/10/earpods.jpg",
+  },
+  {
+    name: "AirPods Pro",
+    pieces: "4",
+    price: 4.939,
+    img: "https://media.idownloadblog.com/wp-content/uploads/2019/10/AirPods-Pro-case-001.jpg",
+  },
+  {
+    name: "AirPods Max",
+    pieces: "7",
+    price: 4.999,
+    img: "https://tse4.mm.bing.net/th?id=OIP.8lbjTyE_NOxDktqpvI3luQHaIO&pid=Api&P=0&h=220",
+  },
+  {
+    name: "AirPods Max",
+    pieces: "5",
+    price: 4.999,
+    img: "https://tse1.mm.bing.net/th?id=OIP.JpAPcMb38u_W-NyOcmN7-gHaIM&pid=Api&P=0&h=220",
+  },
+];
+export const Dropdown = [
+  {
+   title:"Product-Details",
+    content:
+      "The iPhone is a smartphone made by Apple that combines a computer, iPod, digital camera and cellular phone into one device with a touchscreen interface. The iPhone runs the iOS operating system, and in 2021 when the iPhone 13 was introduced, it offered up to 1 TB of storage and a 12-megapixel camera.",
+  },
+  {
+    title:"Nutritions",
+    content:
+      "IPhone Pro Max",
+  },
+  {
+    title:"Reviews",
+    content:
+      "4-reviews iphone",
+  },
+  
+];
